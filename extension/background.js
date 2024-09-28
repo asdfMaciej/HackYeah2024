8590956406fbf0a9a42118264353a10e92a674d0
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2024-09-28 23:06:20 +0200

    feat: gpt-4o vision of current page screenshot

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,15 @@
+var API_KEY = null;
+const restoreOptions = () => {
+    chrome.storage.sync.get(
+      { api_key: null},
+      (items) => {
+        API_KEY = items.api_key;
+        console.log("OpenAI api key loaded!"); 
+	}
+    );
+  };
+
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log('received message: ', message);
     if (message.action === 'setBadge') {
@@ -9,18 +21,76 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === 'BUTTON_CLICKED') {
       console.log('Button was clicked in the popup!');
-  
-      // Perform any background actions here, e.g., interacting with tabs or modifying data
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.scripting.executeScript({
-          target: { tabId: tabs[0].id },
-          function: () => alert('Background script received the button click!')
+
+      chrome.tabs.captureVisibleTab(null, { format: 'png' }, function (dataUrl) {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError.message);
+          sendResponse({ screenshotUrl: null });
+          return;
+        }
+        
+        fetchChatCompletion([{"role": "system", "content": "What can you see on the user image?"}, {"role": "user", "content": [
+            {type: "text", "text": ""},
+            {type: "image_url", "image_url": {url: dataUrl}}
+        ]
+        }], 
+            API_KEY, 
+            'gpt-4o'
+        ).then((response) => {
+            sendResponse({ message: response });
+    
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.scripting.executeScript({
+                  target: { tabId: tabs[0].id },
+                  function: (msg_txt) => alert(msg_txt),
+                  args: [response]
+                });
+              });
         });
       });
-  
-      // Send a response back to the popup
-      sendResponse({ message: 'Action performed in the background' });
+
+      
     }
     return true; // Keep the message channel open for the asynchronous response
   });
-  
+
+
+// Fetch data from the OpenAI Chat Completion API
+async function fetchChatCompletion(messages, apiKey, apiModel) {
+    console.log('attempting to fetch openai api');
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
+            },
+            body: JSON.stringify({
+                "messages": messages,
+                "model": apiModel,
+            })
+        });
+
+        if (!response.ok) {
+            console.log('error!');
+            if (response.status === 401) {
+                // Unauthorized - Incorrect API key
+                throw new Error("Looks like your API key is incorrect. Please check your API key and try again.");
+            } else {
+                throw new Error(`Failed to fetch. Status code: ${response.status}`);
+            }
+        }
+
+        let json = await response.json();
+        return json.choices[0].message.content;
+    } catch (error) {
+        // Send a response to the popup script
+        chrome.runtime.sendMessage({ error: error.message });
+
+        console.error(error);
+    }
+}
+
+(async function() {
+	restoreOptions(); 
+})();
--- extension/content.js
+++ extension/content.js
@@ -11,46 +11,8 @@ const restoreOptions = () => {
     );
   };
 
-async function fetchFromAPI() {
-	const tabURL = window.location.href;
-	console.log('fetching rating for: ', tabURL);
-
-	let response; 
-	try {
-		response = await fetch(`${API_URL}/check_url`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			//mode: 'no-cors',
-			body: JSON.stringify({
-				url: tabURL
-			})
-		});
-
-		if (!response.ok) {
-			throw new Error('Response not ok');
-		}
-	} catch {
-		console.log('Failed to fetch rating');
-		return 'Failed to fetch rating';
-	};
-	
-	console.log('fetched from: ', response.url);
-
-	const data = await response.json();
-	console.log('received data: ', data);
-
-	return data;
-}
 
 (async function() {
 	restoreOptions(); // GET API key
-	setTimeout(() => {console.log(API_KEY);}, 2000);
 	chrome.runtime.sendMessage({ action: 'setBadge', text: `...`, color: '#C6A0F6'});
-
-	let rating = 'No rating available';
-	rating = await fetchFromAPI();
-
-	chrome.runtime.sendMessage({ action: 'setBadge', text: `${rating.score}`, color: '#FF0000'});
 })();
