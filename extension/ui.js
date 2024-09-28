a202e3c94aa519e8903c317bb57ed70dbd07495d
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2024-09-29 01:29:31 +0200

    feat: display output in extension GUI

--- extension/ui.js
+++ extension/ui.js
@@ -2,9 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
   let button = document.getElementById('call-openai');
 
   button.addEventListener('click', function () {
+    document.getElementById("response").innerHTML = 'Thinking...';
     // Send a message to the background script
     chrome.runtime.sendMessage({ type: 'BUTTON_CLICKED', user_input: document.querySelector("#user-input").value }, function (response) {
       console.log('response from bg script', response.message); // Response from background script
+      document.getElementById("response").innerHTML = response.message;
     });
   });
 
